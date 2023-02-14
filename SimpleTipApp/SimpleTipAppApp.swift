ddb25b29b0f8387bf9e68176d77dbed68f6e5649
MoonGoon <moongoon.cnu@gmail.com>
2023-02-14 10:36:07 +0900

    Initial Commit

---
+++ SimpleTipApp/SimpleTipAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTipAppApp.swift
+//  SimpleTipApp
+//
+//  Created by cnu on 2023/02/14.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTipAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
